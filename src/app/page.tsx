3bb73cbb4c00ca2cdef039c648c661834de10b1f
G4mingJon4s <Jthelker@gmail.com>
2023-04-28 18:20:30 +0200

    removed example api + npx eslint . --fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { prisma } from '@/server/db'
+import { Inter } from "next/font/google";
+import { prisma } from "@/server/db";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
 	const count = await prisma.session.count();
 
-  return (
-    <div className={inter.className}>Entries: {count}</div>
-  )
+	return (
+		<div className={inter.className}>Entries: {count}</div>
+	);
 }
